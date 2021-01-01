93ba3f7a1119b57cd7c3db041f9fe7fb29d12032
saipraveen50 <72723492+saipraveen50@users.noreply.github.com>
2021-01-01 18:59:39 +0530

    Add files via upload

---
+++ ChattingInterfaces/IChattingService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ChattingInterfaces
+{
+    /// <summary>
+    /// interface for performing chat operation
+    /// </summary>
+    [ServiceContract(CallbackContract =typeof(IClientContracts))]
+    public interface IChattingService
+    {
+        [OperationContract]
+        int Login(string userName);
+        [OperationContract]
+        void Logout();
+    }
+}
